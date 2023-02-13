2894ce1f6851ec01c042486fac84254d59e8a57c
Basil Paul <84876717+basilpaul7@users.noreply.github.com>
2023-02-13 10:32:46 +0530

    Create Trapezoid.py

---
+++ Trapezoid.py
@@ -0,0 +1,13 @@
+#trapezoid
+import numpy as np
+import matplotlib.pyplot as plt
+
+def trapezoid(a, b, c): 
+    n=np.arange(a,b)
+    x=np.zeros_like(n)
+    x[(n>=-c)&(n<=c)]=2
+    x[(n>=a)&(n<-c)]=(-a+n[(n>=a)&(n<-c)])
+    x[(n>c)&(n<-a)]=(-a+n[(n>c)&(n<-a)])
+    plt.plot(n, x)
+    plt.show() 
+trapezoid(-3,4,2)
